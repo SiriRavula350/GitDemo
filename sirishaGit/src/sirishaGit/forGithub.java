7ac251887e1b19abaacd75e9faf0923f58639b02
SiriRavula350 <sirilaxman@gmail.com>
2021-05-23 17:38:58 -0500

    (UserX displayed a variable value)

--- sirishaGit/src/sirishaGit/forGithub.java
+++ sirishaGit/src/sirishaGit/forGithub.java
@@ -2,15 +2,15 @@ package sirishaGit;
 
 
 
-
 public class forGithub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int a=10;
 		System.out.println("Sirisha learning github");
 		System.out.println("Code entered by second userY");
 		System.out.println("changes by user2Yafter downlad");
+		System.out.println(a);
 
 	}
 
