4016c9abe4f99df0b947d8492a51b561d32dd7d9
SiriRavula350 <sirilaxman@gmail.com>
2021-05-23 09:40:10 -0500

    My first commit

---
+++ sirishaGit/src/sirishaGit/forGithub.java
@@ -0,0 +1,15 @@
+package sirishaGit;
+
+
+
+
+public class forGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Sirisha learning github");
+
+	}
+
+}
